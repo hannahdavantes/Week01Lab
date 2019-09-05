b48f02e571eb94fbd2656d53d3193c9e511cb816
652343 <652343@ELF8IT221633.ACDM.DS.SAIT.CA>
2019-09-05 17:22:28 -0600

    Code HelloWorld2 Hello World 2 coded.

--- src/helloworld2/HelloWorld2.java
+++ src/helloworld2/HelloWorld2.java
@@ -15,7 +15,7 @@ public class HelloWorld2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.print("Hello World 2!");
+        System.out.println("Hello World 2!");
     }
     
 }
